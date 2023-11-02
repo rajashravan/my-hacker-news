721b1e9be3cff3f81db6876fbbd5a6cfb94a2c2d
Raja Shravan <raja@banter.shopping>
2023-11-02 17:19:02 -0400

    Initial Commit

---
+++ my-hacker-news/my_hacker_newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_hacker_newsApp.swift
+//  my-hacker-news
+//
+//  Created by Raja Shravan on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct my_hacker_newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
